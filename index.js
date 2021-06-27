b9b662535969889abd5a744dc664ef73f6290f39
Nacho <isepulm@gmail.com>
2021-06-28 01:31:39 +0200

    rickroll

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js")
 const client = new Discord.Client()
+const ytdl = require("ytdl-core")
 
 client.on("ready", () => {
 	console.log(`Logged in as ${client.user.tag}!`)
@@ -10,5 +11,25 @@ client.login(process.env.TOKEN)
 client.on("message", (msg) => {
 	if (msg.content === "ping") {
 		msg.reply("pong")
+	}
+})
+
+client.on("message", async (message) => {
+	// Voice only works in guilds, if the message does not come from a guild,
+	// we ignore it
+	if (!message.guild) return
+
+	if (message.content === "teta") {
+		// Only try to join the sender's voice channel if they are in one themselves
+		if (message.member.voice.channel) {
+			const connection = await message.member.voice.channel.join()
+			connection.play(
+				ytdl("https://www.youtube.com/watch?v=dQw4w9WgXcQ", {
+					filter: "audioonly",
+				})
+			)
+		} else {
+			message.reply("You need to join a voice channel first!")
+		}
 	}
 })
